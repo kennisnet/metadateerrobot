c2e96e6e04b95b19c7fe0be54d3a9f4b1c2e17be
Wim Muskee <wimmuskee@gmail.com>
2014-05-12 14:51:57 +0200

    replace tabs by spaces in common.py

--- common/common.py
+++ common/common.py
@@ -1,30 +1,30 @@
 # -*- coding: utf-8 -*-
 
 def getConfig(configfile,section):
-	import ConfigParser
-	Config = ConfigParser.ConfigParser()
-	Config.read(configfile)
+    import ConfigParser
+    Config = ConfigParser.ConfigParser()
+    Config.read(configfile)
 
-	config_options = {}
-	options = Config.options(section)
+    config_options = {}
+    options = Config.options(section)
 
-	for option in options:
-		try:
-			config_options[option] = Config.get(section, option)
-			if config_options[option] == -1:
-				DebugPrint("skip: %s" % option)
-		except:
-			print("exception on %s!" % option)
-			config_options[option] = None
+    for option in options:
+        try:
+            config_options[option] = Config.get(section, option)
+            if config_options[option] == -1:
+                DebugPrint("skip: %s" % option)
+        except:
+            print("exception on %s!" % option)
+            config_options[option] = None
 
-	return config_options
+    return config_options
 
 
 def getRe(x):
-	return {
-		'uuid': "^[0-F]{8}-[0-F]{4}-[0-F]{4}-[0-F]{4}-[0-F]{12}$",
-		'dash': "-",
-		}.get(x, False)
+    return {
+        'uuid': "^[0-F]{8}-[0-F]{4}-[0-F]{4}-[0-F]{4}-[0-F]{12}$",
+        'dash': "-",
+        }.get(x, False)
 
 
 """ Dynamically import a method """
