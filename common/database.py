938ceea5e894d78a3133f12b5636a8759330a293
Wim Muskee <wimmuskee@gmail.com>
2014-05-13 07:49:57 +0200

    Make sure all input keywords are stripped, also replace tabs for spaces.

--- common/database.py
+++ common/database.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
 
-def getKeywordId(DB,keyword):
-	c = DB.cursor()
-	query = "SELECT id FROM keywords WHERE keyword = %s"
-	c.execute(query,(keyword))
-	row = c.fetchone()
+def getKeywordId(DB,keyword_raw):
+    keyword = keyword_raw.strip()
 
-	if row:
-		keyword_id = row[0]
-	else:
-		query = "INSERT INTO keywords (keyword) VALUES(%s)"
-		c.execute(query,(keyword))
-		keyword_id = c.lastrowid
+    c = DB.cursor()
+    query = "SELECT id FROM keywords WHERE keyword = %s"
+    c.execute(query,(keyword))
+    row = c.fetchone()
 
-	c.close()
-	return keyword_id
+    if row:
+        keyword_id = row[0]
+    else:
+        query = "INSERT INTO keywords (keyword) VALUES(%s)"
+        c.execute(query,(keyword))
+        keyword_id = c.lastrowid
+
+    c.close()
+    return keyword_id
